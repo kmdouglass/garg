737a3f6a61813da8fbeab8ea8519177d818b7d8c
kmdouglass <kyle.m.douglass@gmail.com>
2017-01-22 18:58:20 +0100

    Fix error in setup.py for PyPI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author       = 'Kyle M. Douglass',
   author_email = 'kyle.m.douglass@gmail.com',
   url          = 'https://github.com/kmdouglass/garg',
-  download_url = 'https://github.com/kmdouglass/garg/tarball/0.0.0', 
+  download_url = 'https://github.com/kmdouglass/garg/tarball/v0.0.0', 
   keywords     = ['gui', 'signature', 'function', 'arguments'],
   classifiers  = [],
 )
