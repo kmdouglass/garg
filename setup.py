572ae3548aa2ad58354ae15b3a1bac4fc1acd89c
kmdouglass <kyle.m.douglass@gmail.com>
2017-04-06 21:51:34 +0200

    Updated version number in setup.py.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name         = 'Garg',
   py_modules   = ['garg'],
-  version      = '0.0.0',
+  version      = '0.1.0',
   description  = 'GUI-based function argument assignment in Python',
   author       = 'Kyle M. Douglass',
   author_email = 'kyle.m.douglass@gmail.com',
   url          = 'https://github.com/kmdouglass/garg',
-  download_url = 'https://github.com/kmdouglass/garg/tarball/v0.0.0', 
+  download_url = 'https://github.com/kmdouglass/garg/tarball/v0.1.0',
   keywords     = ['gui', 'signature', 'function', 'arguments'],
   classifiers  = [],
 )
